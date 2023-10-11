5b46e56a7bd5a505228e42b305c1abe407e639c8
Timothy Orlov <orlovt7@yandex.ru>
2023-10-11 12:41:45 -0500

    added a func to generate labels
    needs work on the "system" part

---
+++ preprocessing/label_generation.py
@@ -0,0 +1,19 @@
+import openai 
+import os 
+from dotenv import load_dotenv
+
+# openai.api_key = "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"
+
+load_dotenv()
+openai.api_key = os.environ.get("API-KEY")
+
+def create_question(string): 
+    response = openai.ChatCompletion.create(
+       model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "There is a relation Question --> Answer, I am providing you the answer, give me a the question"},
+            {"role": "user", "content": string}])
+    return response['choices'][0]['message']['content']
+
+if __name__ == "__main__": 
+    print(create_question("print(\"Helo World\")"))
